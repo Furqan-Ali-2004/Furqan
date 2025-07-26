2d5e8e6aa94dfb374467a362941b1c4356a0d754
Furqan-Ali-2004 <furqangraphy2004@gmail.com>
2025-07-26 10:32:22 +0500

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+// tailwind.config.js
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      keyframes: {
+        blob: {
+          "0%": { transform: "scale(1)" },
+          "33%": { transform: "scale(1.2)" },
+          "66%": { transform: "scale(0.8)" },
+          "100%": { transform: "scale(1)" },
+        },
+      },
+      animation: {
+        blob: "blob 10s infinite",
+      },
+      backgroundImage: {
+        "skills-gradient":
+          "linear-gradient(38.73deg, rgba(60,179,113, 0.15) 0%, rgba(60,179,113, 0) 50%), linear-gradient(141.27deg, rgba(0,128,0, 0) 50%, rgba(0,128,0, 0.15) 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
